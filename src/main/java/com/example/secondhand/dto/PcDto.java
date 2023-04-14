3b7034a86573d55b4cd3f5b850c0527bd171e32f
Yusuf Kilic <yuzarsifkilic@gmail.com>
2023-04-14 15:17:32 +0300

    fixed PcDto

--- src/main/java/com/example/secondhand/dto/PcDto.java
+++ src/main/java/com/example/secondhand/dto/PcDto.java
@@ -3,6 +3,12 @@ package com.example.secondhand.dto;
 import com.example.secondhand.model.Pc;
 
 public record PcDto(Long id,
+                    String shortDetails,
+                    float price,
+                    boolean isSold,
+                    String details,
+                    ProductBrandDto productBrand,
+                    SellerDto seller,
                     CpuDto cpu,
                     double cpuSpeed,
                     GpuDto gpu,
@@ -16,6 +22,12 @@ public record PcDto(Long id,
     public static PcDto convert(Pc from) {
         return new PcDto(
                 from.getId(),
+                from.getShortDetails(),
+                from.getPrice(),
+                from.isSold(),
+                from.getDetails(),
+                ProductBrandDto.convert(from.getProductBrand()),
+                SellerDto.convert(from.getSeller()),
                 CpuDto.convert(from.getCpu()),
                 from.getCpuSpeed(),
                 GpuDto.convert(from.getGpu()),
